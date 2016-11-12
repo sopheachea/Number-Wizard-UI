4050fb0130a02cdb174796e56820fa1676a02205
sophea_chea <cheasophea101@gmail.com>
2016-11-12 18:46:24 +0700

    first commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name){
+		Debug.Log ("Level load request for: " + name);
+
+		// Load scene
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit request.");
+
+		// this method is effects only for PC, Mac, and Linux
+		// it's will not effects for mobiles app, and will eject from play store or app store
+		// if you add this method to your game.
+		// It also not effects on debug mode.
+		Application.Quit ();
+	}
+}
